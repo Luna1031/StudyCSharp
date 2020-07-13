963fd3c0c4bb3f2c8a3d1c389420d521021ccc3f
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-07-13 17:20:53 +0900

    13072020

---
+++ MyStockSystem/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MyStockSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MtlStockAnals = new MetroFramework.Controls.MetroTile();
+            this.MltInvestSimul = new MetroFramework.Controls.MetroTile();
+            this.MtlSearchItem = new MetroFramework.Controls.MetroTile();
+            this.SuspendLayout();
+            // 
+            // MtlStockAnals
+            // 
+            this.MtlStockAnals.Location = new System.Drawing.Point(329, 63);
+            this.MtlStockAnals.Name = "MtlStockAnals";
+            this.MtlStockAnals.Size = new System.Drawing.Size(150, 150);
+            this.MtlStockAnals.Style = MetroFramework.MetroColorStyle.Orange;
+            this.MtlStockAnals.TabIndex = 0;
+            this.MtlStockAnals.Text = "주식정보분석";
+            this.MtlStockAnals.TileImage = global::MyStockSystem.Properties.Resources.analysis2;
+            this.MtlStockAnals.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MtlStockAnals.UseTileImage = true;
+            // 
+            // MltInvestSimul
+            // 
+            this.MltInvestSimul.Location = new System.Drawing.Point(329, 219);
+            this.MltInvestSimul.Name = "MltInvestSimul";
+            this.MltInvestSimul.Size = new System.Drawing.Size(300, 150);
+            this.MltInvestSimul.Style = MetroFramework.MetroColorStyle.Lime;
+            this.MltInvestSimul.TabIndex = 0;
+            this.MltInvestSimul.Text = "투자시뮬레이션";
+            this.MltInvestSimul.TileImage = global::MyStockSystem.Properties.Resources.simulator2;
+            this.MltInvestSimul.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MltInvestSimul.UseTileImage = true;
+            // 
+            // MtlSearchItem
+            // 
+            this.MtlSearchItem.Location = new System.Drawing.Point(23, 63);
+            this.MtlSearchItem.Name = "MtlSearchItem";
+            this.MtlSearchItem.Size = new System.Drawing.Size(300, 150);
+            this.MtlSearchItem.Style = MetroFramework.MetroColorStyle.Pink;
+            this.MtlSearchItem.TabIndex = 0;
+            this.MtlSearchItem.Text = "주식정보검색";
+            this.MtlSearchItem.TileImage = global::MyStockSystem.Properties.Resources.marketing2;
+            this.MtlSearchItem.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MtlSearchItem.UseTileImage = true;
+            this.MtlSearchItem.Click += new System.EventHandler(this.MtlSearchItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.MtlStockAnals);
+            this.Controls.Add(this.MltInvestSimul);
+            this.Controls.Add(this.MtlSearchItem);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Resizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Style = MetroFramework.MetroColorStyle.Magenta;
+            this.Text = "주식분석시스템";
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile MtlSearchItem;
+        private MetroFramework.Controls.MetroTile MtlStockAnals;
+        private MetroFramework.Controls.MetroTile MltInvestSimul;
+    }
+}
+
---
+++ MyStockSystem/SubItems/SearchItemForm.cs
@@ -0,0 +1,99 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MyStockSystem.SubItems
+{
+    public partial class SearchItemForm : MetroForm
+    {
+        public SearchItemForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SerachItemForm_Load(object sender, EventArgs e)
+        {
+            DgvSearchItems.Font = new Font(@"NanumGothic", 9, FontStyle.Italic);
+        }
+
+        private void MtlBack_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            MainForm main = new MainForm();
+            main.Location = this.Location;
+            main.ShowDialog();
+
+            this.Close();
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient { Encoding = Encoding.UTF8 };
+            XmlDocument doc = new XmlDocument();
+
+            StringBuilder str = new StringBuilder();
+            str.Append("http://api.seibro.or.kr/openapi/service/StockSvc/getStkIsinByNmN1");     // OpenAPI 기본 
+            str.Append("?serviceKey=8zM4bOCfp2qSTfRECbNqx18v2XNzcH74SIy5u%2FqNnOuYGNesMgjfOWFce24yF4MexR1w5qo70soTRJHERI2IZg%3D%3D");   // 최초의 파라미터값은 '?', 다음부터는 '&'
+            str.Append($"&secnNm={TxtSearchItem.Text}");    // 검색어
+            str.Append("&numOfRows=200");                   // 읽어 올 데이터 수
+            str.Append("&pageNo=1");                        // 페이지 번호
+            str.Append("&martTpcd=11");                     // 주식시장 종류 11 유가증권
+
+            string xml = wc.DownloadString(str.ToString());
+            doc.LoadXml(xml);
+
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList items = doc.GetElementsByTagName("item");
+
+            DgvSearchItems.Rows.Clear();
+
+            try
+            {
+                foreach (XmlNode item in items)
+                {
+                    DgvSearchItems.Rows.Add(item["isin"].InnerText,                                                         // isin 표준코드
+                                            item["issuDt"] == null ? string.Empty : item["issuDt"].InnerText,               // 주식발행일자
+                                            item["korSecnNm"].InnerText,                                                    // 한글종목명
+                                            item["secnKacdNm"].InnerText,                                                   // 보통주/우선주
+                                            item["shotnIsin"].InnerText);                                                   // 단축코드
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show($"에러 발생 : {ex.Message}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+
+            DgvSearchItems.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+        }
+
+        private void TxtSearchItem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                BtnSearch_Click(sender, new EventArgs());
+            }
+        }
+    }
+}
