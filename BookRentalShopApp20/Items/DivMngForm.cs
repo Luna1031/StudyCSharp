a3bccd111bb77d3310632130a1e23e10e63de2da
Luna1031 <60143547+Luna1031@users.noreply.github.com>
2020-07-14 16:10:40 +0900

    14072020

---
+++ BookRentalShopApp20/Items/DivMngForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BookRentalShopApp20.Items
+{
+    public partial class DivMngForm : MetroForm
+    {
+        readonly string strTblName = "divTbl";
+        public DivMngForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DivMngForm_Load(object sender, EventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void UpdateData()
+        {
+            using (MySqlConnection conn = new MySqlConnection(Commons.CONNSTR))
+            {
+                string strQuery = $"SELECT Division, Names FROM {strTblName} ";
+                conn.Open();
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(strQuery, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, strTblName);
+
+                GrdDivTbl.DataSource = ds;
+                GrdDivTbl.DataMember = strTblName;
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BookRentalShopApp20/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using BookRentalShopApp20.Items;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace BookRentalShopApp20
+{
+    public partial class MainForm : MetroForm
+    {
+        public object FormWidowState { get; private set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            login.ShowDialog();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MetroMessageBox.Show(this, "종료하시겠습니다?", "종료", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes)
+            {
+                e.Cancel = false;
+                Environment.Exit(0);
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        private void MnuItemCodeMng_Click(object sender, EventArgs e)
+        {
+            DivMngForm form = new DivMngForm();
+            form.MdiParent = this;
+            form.Text = "구분코드 관리";
+            form.Dock = DockStyle.Fill;
+            form.Show();
+            form.WindowState = FormWindowState.Maximized;
+        }
+
+        private void MnuItemExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
